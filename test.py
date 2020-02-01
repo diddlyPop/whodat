7c27b40dfe7192407e5006c1f7d2e61af8059710
daytonschuh <47435241+daytonschuh@users.noreply.github.com>
2020-01-31 17:00:36 -0800

    Temporary testing gui

---
+++ test.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import sys
+import PySimpleGUI as sg
+
+# Usage of Tabs in PSG
+#
+# sg.set_options(background_color='cornsilk4',
+#         element_background_color='cornsilk2',
+#         input_elements_background_color='cornsilk2')
+
+# Color theme
+sg.theme('Dark Amber')
+
+# TODO: Everything settings using sg.set_options
+setup_layout = [[sg.Text('Configure settings and finalize setup')]]
+
+'''
+TODO: Write code for buttons
+Train takes a picture and allows you to enter information; verify with sg.Submit(), go back with sg.Cancel()
+Test takes a picture, makes a guess and then asks if it is correct using sg.PopupYesNo()
+'''
+train_layout = [[sg.Text('Train your Pi to recognize your friends and family')],
+                [sg.Button('Train', pad=(80,100)), sg.Button('Test')]]
+
+'''
+TODO: Figure out how to link this to Twilio and authenticate
+Password field infinite. That's no good
+'''
+twilio_layout = [[sg.Text('Link your Twilio to receive message updates from WhoDat')],
+                 [sg.Text('Log in\t'), sg.Input()],
+                 [sg.Text('Password'), sg.Input(password_char="*", key='-USER_PASS-')],
+                 [sg.Button('Log in')]]
+
+# TODO: Fill in with information
+about_layout = [[sg.Text('About WhoDat')], [sg.Text('\tINFOMATSION')]]
+
+# This is our predefined layout using our instances from above
+layout = [[sg.TabGroup([[sg.Tab('Setup', setup_layout),
+                         sg.Tab('Train', train_layout),
+                         sg.Tab('Twilio', twilio_layout),
+                         sg.Tab('About', about_layout)]], key='-tab_group1-', tab_location='top', selected_title_color='yellow')]]
+
+# This is the window generated sg.Window(Title of the window, our predefined layout)
+window = sg.Window('WhoDat').Layout(layout)
+
+while True:
+    event, values = window.Read()
+    #print(len(values['-USER_PASS-']))
+    #print(len(values['-tab_group1-']))
+
+    '''
+    TODO: Update on key input or tab switch
+    ** Updating shown in terminal recognizes what it's SUPPOSED to do,
+        but only updates the gui with button press
+    '''
+    # ---- This is supposed to limit character input for password field ----
+    # ---- by reducing the length of input by 1 any time it exceeds 10. ----
+    if len(values['-USER_PASS-']) > 10:
+        window.Element('-USER_PASS-').Update(values['-USER_PASS-'][:-1])
+    # ----------------------------------------------------------------------
+
+    print(event,values)
+
+    if event is 'Log in':
+        print('You clicked log in')
+    if event is None:           # always, always give a way out!
+        break
+window.close()
