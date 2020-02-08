7e906e0837ef2893386da4b45ecb9646dc07cb5b
Kyle Guss <kyleguss@icloud.com>
2020-02-08 12:37:50 -0800

    working on v0.1 release

--- src/whocam.py
+++ src/whocam.py
@@ -12,20 +12,7 @@ class WhoCam:
             pass
 
     def start(self):
-        cap = cv.VideoCapture(0)
-
-        while (True):
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-
-            # Our operations on the frame come here
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-            # Display the resulting frame
-            if self.launchDisplay:
-                cv.imshow('frame', gray)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
+        pass
 
     def close(self):
         self.window.close()
